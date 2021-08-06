02b555e2bcfa1ebeffdf41cb4307c3dd3360c531
GoldenPavilion <rcorystine@gmail.com>
2021-08-06 17:08:37 -0400

    Done.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  tutorials.map(tutorial => tutorial.split(" ").map(
+  return tutorials.map(tutorial => tutorial.split(" ").map(
     word => word[0].toUpperCase() + word.slice(1)
   ).join(" "))
 }
